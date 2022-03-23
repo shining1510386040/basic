a99c15f0853f8049963d2b4bf77f40af12eeffd1
shining1510386040 <1510386040@qq.com>
2022-03-23 12:01:16 +0800

    接口异步调用测试

---
+++ opencv-with-java/src/main/java/com/demo/springboot/opencvwithjava/OpencvWithJavaApplication.java
@@ -0,0 +1,13 @@
+package com.demo.springboot.opencvwithjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpencvWithJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpencvWithJavaApplication.class, args);
+    }
+
+}
